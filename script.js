643ff16572c4d0a99ea6df2ac91bdc0d0e880690
gopalptl14 <gopalptl2350@gmail.com>
2023-05-27 23:58:15 +0530

    solve question

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+    for(const ss of strong){
+        ss.style.color = "green";
+    }
 
 }
 
-
+const strong = document.getElementsByTagName("strong");
+// console.log(strong);
 function return_normal() {
     //Write your code here
-
+for(const ss of strong){
+    ss.style.color="black";
+}
     
 }
